517818b21106431b087903c090f709a8d84803eb
Alex Yip <alex.yip@adhocteam.us>
2021-04-24 07:26:02 -0700

    Modified test to use realistic user id

--- test/modal-builder.test.js
+++ test/modal-builder.test.js
@@ -7,12 +7,12 @@ chai.use(assertArrays);
 const expect = chai.expect;
 
 describe('Modal Builder', () => {
-  it('should build a modal with supplied username', () => {
-      const modal = modalBuilder.buildSupportModal('johnsmith');
+  it('should build a modal with supplied user id', () => {
+      const modal = modalBuilder.buildSupportModal('U01T9CL8PEK');
 
       const initial_users = modal.blocks[2].element.initial_users;
 
       expect(initial_users).to.have.lengthOf(1);
-      expect(initial_users).to.be.equalTo(['johnsmith']);
+      expect(initial_users).to.be.equalTo(['U01T9CL8PEK']);
   });
 });

4aaba7bf97526828aac3661ec24bf8e041980407
Alex Yip <alex.yip@adhocteam.us>
2021-05-04 06:47:26 -0700

    Commenting and clean up

--- src/workflow-handler.js
+++ src/workflow-handler.js
@@ -5,34 +5,64 @@ const SUPPORT_CHANNEL_ID = process.env.SLACK_SUPPORT_CHANNEL;
 
 module.exports = function (app, logger) {
   const ws = new WorkflowStep(WORKFLOW_CALLBACK_ID, {
-    edit: async ({ ack, step, configure }) => {
-      await ack();
+    /**
+     * edit() gets called when a workflow administrator adds/edits
+     * the platform support app's workflow step in workflow builder
+     * and is used to configure this step.
+     *
+     * Note: Since this workflow does nothing but send a message,
+     * no configuration is necessary.
+     *
+     * @param {object} param Slack Workflow Edit Parameter
+     */
+    edit: async ({ ack, configure }) => {
+      try {
+        await ack();
 
-      logger.info(`${WORKFLOW_CALLBACK_ID} workflow edit callback.`);
+        logger.debug(`${WORKFLOW_CALLBACK_ID} workflow edit callback.`);
 
-      const blocks = [];
+        const blocks = [];
 
-      await configure({ blocks });
+        await configure({ blocks });
+      } catch (error) {
+        logger.error(error);
+      }
     },
-    save: async ({ ack, step, view, update }) => {
-      await ack();
+    /**
+     * save() gets called when a workflow adminstrator saves
+     * the workflow step in workflow builder.
+     *
+     * Note: Since this workflow does nothing but send a message,
+     * no configuration is saved.
+     *
+     * @param {object} param Slack Workflow Save Parameter
+     */
+    save: async ({ ack, update }) => {
+      try {
+        await ack();
 
-      logger.info(`${WORKFLOW_CALLBACK_ID} workflow save callback.`);
+        logger.debug(`${WORKFLOW_CALLBACK_ID} workflow save callback.`);
 
-      await update({});
+        await update({});
+      } catch (error) {
+        logger.error(error);
+      }
     },
-    execute: async (obj) => {
-      //   console.log(JSON.stringify(obj));
-      const { step, complete, fail, client, body, payload, event } = obj;
-      const { inputs } = step;
-
+    /**
+     * execute() is called when a workflow step is executed and
+     * data (inputs) from a previous step gets sent to the next
+     * step (outputs).
+     *
+     * This workflow step posts a message to the user about how
+     * to submit a platform support request.
+     *
+     * @param {object} param Slack Workflow Execute Parameter
+     */
+    execute: async ({ complete, client }) => {
       const outputs = {};
 
-      logger.info('Executing...');
-
       try {
-        //U01T9CL8PEK
-        logger.info(`${WORKFLOW_CALLBACK_ID} workflow step executed.`);
+        logger.debug(`${WORKFLOW_CALLBACK_ID} workflow step executed.`);
 
         client.chat.postMessage({
           channel: SUPPORT_CHANNEL_ID,
@@ -69,18 +99,11 @@ module.exports = function (app, logger) {
             },
           ],
         });
-
-        // Call views.open with the built-in client
-        //util.buildSupportModal(client, body.user_id, body.trigger_id);
       } catch (error) {
         logger.error(error);
       }
 
-      // if everything was successful
       await complete({ outputs });
-
-      // if something went wrong
-      // fail({ error: { message: "Just testing step failure!" } });
     },
   });
 

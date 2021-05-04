4e3f364d3581956cd35e20fc0f234f0f29c5421b
Alex Yip <alex.yip@adhocteam.us>
2021-05-04 09:40:35 -0700

    Auto formatting

--- src/api/slack/block-kit/modal-builder.js
+++ src/api/slack/block-kit/modal-builder.js
@@ -1,97 +1,97 @@
 const buildDropDownOptions = (options) => {
-  return options.map(option => {
-      return {
-        text: {
-          type: "plain_text",
-          text: option.text,
-          emoji: true,
-        },
-        value: option.value,
-      };
+  return options.map((option) => {
+    return {
+      text: {
+        type: 'plain_text',
+        text: option.text,
+        emoji: true,
+      },
+      value: option.value,
+    };
   });
-}
+};
 
 const buildSupportModal = (user, options) => {
   return {
-    type: "modal",
-    callback_id: "support_modal_view",
+    type: 'modal',
+    callback_id: 'support_modal_view',
     submit: {
-      type: "plain_text",
-      text: "Submit",
+      type: 'plain_text',
+      text: 'Submit',
       emoji: true,
     },
     close: {
-      type: "plain_text",
-      text: "Cancel",
+      type: 'plain_text',
+      text: 'Cancel',
       emoji: true,
     },
     title: {
-      type: "plain_text",
-      text: "How can we help?",
+      type: 'plain_text',
+      text: 'How can we help?',
       emoji: true,
     },
     blocks: [
       {
-        type: "section",
+        type: 'section',
         text: {
-          type: "plain_text",
+          type: 'plain_text',
           text:
             ":wave: Hey there!\n\nPlease fill out the form below and we'll route your support request to the right team.",
           emoji: true,
         },
       },
       {
-        type: "divider",
+        type: 'divider',
       },
       {
-        type: "input",
-        block_id: "topic",
+        type: 'input',
+        block_id: 'topic',
         element: {
-          type: "static_select",
+          type: 'static_select',
           placeholder: {
-            type: "plain_text",
-            text: "Select an item",
+            type: 'plain_text',
+            text: 'Select an item',
             emoji: true,
           },
           options: buildDropDownOptions(options),
-          action_id: "selected",
+          action_id: 'selected',
         },
         label: {
-          type: "plain_text",
-          text: "I need help from",
+          type: 'plain_text',
+          text: 'I need help from',
           emoji: true,
         },
       },
       {
-        type: "input",
-        block_id: "summary",
+        type: 'input',
+        block_id: 'summary',
         label: {
-          type: "plain_text",
-          text: "Summary of request",
+          type: 'plain_text',
+          text: 'Summary of request',
           emoji: true,
         },
         element: {
-          type: "plain_text_input",
+          type: 'plain_text_input',
           multiline: true,
-          action_id: "value",
+          action_id: 'value',
         },
       },
       {
-        type: "input",
-        block_id: "users_requesting_support",
+        type: 'input',
+        block_id: 'users_requesting_support',
         element: {
-          type: "multi_users_select",
+          type: 'multi_users_select',
           placeholder: {
-            type: "plain_text",
-            text: "Select users",
+            type: 'plain_text',
+            text: 'Select users',
             emoji: true,
           },
-          action_id: "users",
+          action_id: 'users',
           initial_users: [user],
         },
         label: {
-          type: "plain_text",
-          text: "User(s) requesting support",
+          type: 'plain_text',
+          text: 'User(s) requesting support',
           emoji: true,
         },
       },

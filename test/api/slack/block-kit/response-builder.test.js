4c1379f21af9bb4d10520fe81e8bd901e7e31dad
Alex Yip <alex.yip@adhocteam.us>
2021-05-07 05:05:20 -0700

    Fixed unit tests

--- test/api/slack/block-kit/response-builder.test.js
+++ test/api/slack/block-kit/response-builder.test.js
@@ -3,20 +3,27 @@ const responseBuilder = require('../../../../src/api/slack/block-kit/response-bu
 
 describe('Response Builder', () => {
   it('should populate a response message correctly', () => {
+    const ticketId = 'abc123';
     const userId = 'alex.yip';
     const selectedTeam = 'FE Tools';
     const summaryDescription = 'I need a PR Reviewed';
+    const mention = 'alex.yip';
+    const team = null;
 
     let response = responseBuilder.buildSupportResponse(
+      ticketId,
       userId,
       selectedTeam,
-      summaryDescription
+      summaryDescription,
+      mention,
+      team
     );
 
     expect(response[0].text.text).to.equal(
       "Hey there <@alex.yip>! We've recieved your Platform support request."
     );
-    expect(response[1].text.text).to.equal('*Need help with*\nFE Tools');
+    expect(response[1].text.text).to.equal('*Need help from*\nFE Tools');
     expect(response[2].text.text).to.equal('*Summary*\nI need a PR Reviewed');
+    expect(response[3].text.text).to.equal('Assigned to: <@alex.yip>');
   });
 });
--- test/util.test.js
+++ test/util.test.js
@@ -1,5 +1,5 @@
 const expect = require('chai').expect;
-const util = require('../src/util');
+const util = require('../src/api/slack/util');
 
 describe('Util', () => {
   let sut;
@@ -13,7 +13,8 @@ describe('Util', () => {
   });
 
   it('build a message link correctly', () => {
-    //https://adhoc.slack.com/archives/C01UZ7SULTX/p1620056853018500
+    // Example Link:
+    // https://adhoc.slack.com/archives/C01UZ7SULTX/p1620056853018500
     const channel = 'C01UZ7SULTX';
     const messageId = '1620055651.018100';
 
@@ -22,5 +23,14 @@ describe('Util', () => {
     expect(link).to.equal(
       'https://adhoc.slack.com/archives/C01UZ7SULTX/p1620055651018100'
     );
+  });
+
+  it('should return an empty string with a null channel', () => {
+    const channel = null;
+    const messageId = null;
+
+    const link = sut.createMessageLink(channel, messageId);
+
+    expect(link).to.equal('');
   });
 });

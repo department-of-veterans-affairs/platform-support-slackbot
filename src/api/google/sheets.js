121ffe5c741f7d835f50f82c87150a28de0577d8
Alex Yip <alex.yip@adhocteam.us>
2021-05-07 06:40:54 -0700

    Refactored google sheets module

--- src/api/google/sheets.js
+++ src/api/google/sheets.js
@@ -14,7 +14,7 @@ module.exports = function (logger) {
    * @param {string} spreadsheetId
    * @returns Google Spreadsheet Instance
    */
-  const getGoogleSheet = async (spreadsheetId) => {
+  sheets.getGoogleSheet = async (spreadsheetId) => {
     const doc = new GoogleSpreadsheet(spreadsheetId);
 
     // Authentication using Google Service Account (See client_secret.json)
@@ -27,25 +27,45 @@ module.exports = function (logger) {
   };
 
   /**
+   * Gets all rows for the Google Teams Sheet
+   * @returns Google Sheet Rows
+   */
+  sheets.getTeamsSheetRows = async () => {
+    const doc = await sheets.getGoogleSheet(process.env.TEAMS_SPREADSHEET_ID);
+
+    const sheet = doc.sheetsByIndex[0];
+
+    return await sheet.getRows();
+  };
+
+  /**
+   * Gets all rows for the Google Responses Sheet
+   * @returns Google Sheet Rows
+   */
+  sheets.getResponseSheetRows = async () => {
+    const doc = await sheets.getGoogleSheet(
+      process.env.RESPONSES_SPREADSHEET_ID
+    );
+
+    const sheet = doc.sheetsByIndex[0];
+
+    return await sheet.getRows();
+  };
+
+  /**
    * Reads the team Google Spreadsheet and returns an array of
    * teams and associated values.
    * @returns Array of text/values
    */
   sheets.getTeams = async () => {
-    const doc = await getGoogleSheet(process.env.TEAMS_SPREADSHEET_ID);
+    const rows = await sheets.getTeamsSheetRows();
 
-    const sheet = doc.sheetsByIndex[0];
-
-    const rows = await sheet.getRows();
-
-    let result = [];
-
-    for (let i = 0; i < sheet.rowCount; i++) {
-      if (!rows[i]) break;
-      result.push({ text: rows[i].Title, value: rows[i].Id });
-    }
-
-    return result;
+    return rows.map((row) => {
+      return {
+        text: row.Title,
+        value: row.Id,
+      };
+    });
   };
 
   /**
@@ -54,11 +74,7 @@ module.exports = function (logger) {
    * @returns
    */
   sheets.getTeamById = async (teamId) => {
-    const doc = await getGoogleSheet(process.env.TEAMS_SPREADSHEET_ID);
-
-    const sheet = doc.sheetsByIndex[0];
-
-    const rows = await sheet.getRows();
+    const rows = await sheets.getTeamsSheetRows();
 
     return rows[teamId - 1];
   };
@@ -85,7 +101,9 @@ module.exports = function (logger) {
     messageLink,
     dateTime = new Date()
   ) => {
-    const doc = await getGoogleSheet(process.env.RESPONSES_SPREADSHEET_ID);
+    const doc = await sheets.getGoogleSheet(
+      process.env.RESPONSES_SPREADSHEET_ID
+    );
 
     const sheet = doc.sheetsByIndex[0];
 
@@ -115,24 +133,9 @@ module.exports = function (logger) {
    * @param {string} messageId - Message ID in Google Sheet to be updated.
    */
   sheets.updateReplyTimeStampForMessage = async (messageId) => {
-    const doc = await getGoogleSheet(process.env.RESPONSES_SPREADSHEET_ID);
+    const rows = await sheets.getResponseSheetRows();
 
-    const sheet = doc.sheetsByIndex[0];
-
-    const rows = await sheet.getRows();
-
-    let row;
-
-    // TODO: Find the best approach for updating the reply time stamp.
-    logger.debug(sheet.rowCount);
-
-    for (let i = 0; i < sheet.rowCount; i++) {
-      if (!rows[i]) break;
-      if (rows[i].MessageId === messageId) {
-        row = rows[i];
-        break;
-      }
-    }
+    const row = rows.find((row) => row.MessageId === messageId);
 
     if (row && row.FirstReplyTimeUTC === '') {
       row.FirstReplyTimeUTC = new Date(Date.now()).toISOString();
@@ -140,6 +143,14 @@ module.exports = function (logger) {
     } else {
       logger.debug('Row not found...');
     }
+  };
+
+  sheets.getMessageByTicketId = async (ticketId) => {
+    const rows = await sheets.getResponseSheetRows();
+
+    const row = rows.find((row) => row.TicketId === ticketId);
+
+    return row.MessageId.replace('msgId:', '');
   };
 
   return sheets;

baf45db09d804cc997faa5c80e61393a001bc691
zach lewis <zlewis@clarityinnovates.com>
2022-07-18 14:33:45 -0500

    add all envs needed

--- src/app.js
+++ src/app.js
@@ -10,18 +10,33 @@ const { App } = require('@slack/bolt');
 const requestHandler = require('./request-handler');
 const workflowHandler = require('./workflow-handler');
 
-botToken = process.env.SLACK_BOT_TOKEN
-slackSecret = process.env.SLACK_SIGNING_SECRET
-appToken = process.env.SLACK_WEB_SOCKET_APP_TOKEN
-slackChannelId = process.env.SLACK_CHANNEL
+SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
+SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET
+SLACK_WEB_SOCKET_APP_TOKEN = process.env.SLACK_WEB_SOCKET_APP_TOKEN
+SLACK_CHANNEL = process.env.SLACK_CHANNEL
+TEAMS_SPREADSHEET_ID = process.env.TEAMS_SPREADSHEET_ID
+RESPONSES_SPREADSHEET_ID=process.env.RESPONSES_SPREADSHEET_ID
+TOPICS_SPREADSHEET_ID=process.env.TOPICS_SPREADSHEET_ID
+
+PAGER_DUTY_API_KEY= process.env.PAGER_DUTY_API_KEY
+GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID
+GOOGLE_PRIVATE_KEY_ID= process.env.GOOGLE_PRIVATE_KEY_ID
 
 
-if (botToken === undefined || slackSecret === undefined || appToken === undefined || slackChannelId === undefined) {
+if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || SLACK_SIGNING_SECRET === undefined || SLACK_BOT_TOKEN === undefined
+|| TEAMS_SPREADSHEET_ID === undefined || RESPONSES_SPREADSHEET_ID === undefined || TOPICS_SPREADSHEET_ID === undefined
+    || PAGER_DUTY_API_KEY === undefined || GOOGLE_CLIENT_ID === undefined || GOOGLE_PRIVATE_KEY_ID === undefined ) {
   console.log("Error: Missing Environment Variables ! Verify the following: ")
   console.log(" - SLACK_SIGNING_SECRET")
   console.log(" - SLACK_BOT_TOKEN")
   console.log(" - SLACK_WEB_SOCKET_APP_TOKEN")
   console.log(" - SLACK_CHANNEL")
+  console.log(" - TEAMS_SPREADSHEET_ID")
+  console.log(" - TOPICS_SPREADSHEET_ID")
+  console.log(" - RESPONSES_SPREADSHEET_ID")
+  console.log(" - PAGER_DUTY_API_KEY")
+  console.log(" - GOOGLE_CLIENT_ID")
+  console.log(" - GOOGLE_PRIVATE_KEY_ID")
 
 } else {
 

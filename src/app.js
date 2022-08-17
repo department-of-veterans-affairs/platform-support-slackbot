804232bb64a1b30588a2be9b455fa2060f3795d3
Kerry Powell <kerry.powell@adhocteam.us>
2022-08-17 11:46:58 -0600

    Update to post when the server goes up and down

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ SLACK_BOT_TOKEN = process.env.SLACK_BOT_TOKEN
 SLACK_SIGNING_SECRET = process.env.SLACK_SIGNING_SECRET
 SLACK_WEB_SOCKET_APP_TOKEN = process.env.SLACK_WEB_SOCKET_APP_TOKEN
 SLACK_CHANNEL = process.env.SLACK_CHANNEL
+SLACK_SUPPORT_TEAM_GROUP = process.env.SLACK_SUPPORT_TEAM_GROUP
 TEAMS_SPREADSHEET_ID = process.env.TEAMS_SPREADSHEET_ID
 RESPONSES_SPREADSHEET_ID=process.env.RESPONSES_SPREADSHEET_ID
 TOPICS_SPREADSHEET_ID=process.env.TOPICS_SPREADSHEET_ID
@@ -25,7 +26,7 @@ GOOGLE_PRIVATE_KEY_ID= process.env.GOOGLE_PRIVATE_KEY_ID
 
 if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || SLACK_SIGNING_SECRET === undefined || SLACK_BOT_TOKEN === undefined
 || TEAMS_SPREADSHEET_ID === undefined || RESPONSES_SPREADSHEET_ID === undefined || TOPICS_SPREADSHEET_ID === undefined
-    || PAGER_DUTY_API_KEY === undefined || GOOGLE_CLIENT_ID === undefined || GOOGLE_PRIVATE_KEY_ID === undefined ) {
+    || PAGER_DUTY_API_KEY === undefined || GOOGLE_CLIENT_ID === undefined || GOOGLE_PRIVATE_KEY_ID === undefined || SLACK_SUPPORT_TEAM_GROUP === undefined ) {
   console.log("Error: Missing Environment Variables ! Verify the following: ")
   console.log(" - SLACK_SIGNING_SECRET")
   console.log(" - SLACK_BOT_TOKEN")
@@ -68,7 +69,40 @@ if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || S
   console.log(`Server running on port ${process.env.PORT || 7172}`);
   await app.start();
 
+    await app.client.chat.postMessage({
+      channel: SLACK_CHANNEL,
+      text: `Platform Support Slack-bot server is running ${new Date()}`,
+      parse: 'full',
+      blocks: [{
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Platform Support Slack-bot server is running ${new Date()}`
+        }
+      }] 
+    })
+
+    let warnProcessStop = async () => {
+      await app.client.chat.postMessage({
+        channel: SLACK_CHANNEL,
+        text: `${SLACK_SUPPORT_TEAM_GROUP} Platform Support Slack-bot server is offline ${new Date()}`,
+        parse: 'full',
+        blocks: [{
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: `${SLACK_SUPPORT_TEAM_GROUP} Platform Support Slack-bot server is offline ${new Date()}`
+          }
+        }] 
+      })
+      return;
+    }
+
     logger.info('⚡️Platform Support Bot is running! ⚡️');
+
+    process.on('SIGTERM', warnProcessStop)
+    process.on('SIGINT', warnProcessStop)
+    process.on('exit', warnProcessStop)
   })();
 
 }

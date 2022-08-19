cbb6e235e4eb931c5d60d1862671488021a097ad
Kerry Powell <kerry.powell@adhocteam.us>
2022-08-19 10:53:42 -0600

    Add updates to server status alerting

--- src/app.js
+++ src/app.js
@@ -16,17 +16,17 @@ SLACK_WEB_SOCKET_APP_TOKEN = process.env.SLACK_WEB_SOCKET_APP_TOKEN
 SLACK_CHANNEL = process.env.SLACK_CHANNEL
 SLACK_SUPPORT_TEAM_GROUP = process.env.SLACK_SUPPORT_TEAM_GROUP
 TEAMS_SPREADSHEET_ID = process.env.TEAMS_SPREADSHEET_ID
-RESPONSES_SPREADSHEET_ID=process.env.RESPONSES_SPREADSHEET_ID
-TOPICS_SPREADSHEET_ID=process.env.TOPICS_SPREADSHEET_ID
+RESPONSES_SPREADSHEET_ID = process.env.RESPONSES_SPREADSHEET_ID
+TOPICS_SPREADSHEET_ID = process.env.TOPICS_SPREADSHEET_ID
 
-PAGER_DUTY_API_KEY= process.env.PAGER_DUTY_API_KEY
-GOOGLE_CLIENT_ID= process.env.GOOGLE_CLIENT_ID
-GOOGLE_PRIVATE_KEY_ID= process.env.GOOGLE_PRIVATE_KEY_ID
+PAGER_DUTY_API_KEY = process.env.PAGER_DUTY_API_KEY
+GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
+GOOGLE_PRIVATE_KEY_ID = process.env.GOOGLE_PRIVATE_KEY_ID
 
 
 if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || SLACK_SIGNING_SECRET === undefined || SLACK_BOT_TOKEN === undefined
-|| TEAMS_SPREADSHEET_ID === undefined || RESPONSES_SPREADSHEET_ID === undefined || TOPICS_SPREADSHEET_ID === undefined
-    || PAGER_DUTY_API_KEY === undefined || GOOGLE_CLIENT_ID === undefined || GOOGLE_PRIVATE_KEY_ID === undefined || SLACK_SUPPORT_TEAM_GROUP === undefined ) {
+  || TEAMS_SPREADSHEET_ID === undefined || RESPONSES_SPREADSHEET_ID === undefined || TOPICS_SPREADSHEET_ID === undefined
+  || PAGER_DUTY_API_KEY === undefined || GOOGLE_CLIENT_ID === undefined || GOOGLE_PRIVATE_KEY_ID === undefined || SLACK_SUPPORT_TEAM_GROUP === undefined) {
   console.log("Error: Missing Environment Variables ! Verify the following: ")
   console.log(" - SLACK_SIGNING_SECRET")
   console.log(" - SLACK_BOT_TOKEN")
@@ -41,7 +41,7 @@ if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || S
 
 } else {
 
-// Initializes bot with Slack API token and signing secret
+  // Initializes bot with Slack API token and signing secret
   const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -49,42 +49,59 @@ if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || S
     appToken: process.env.SLACK_WEB_SOCKET_APP_TOKEN,
   });
 
-// Handles Slack Requests
+  // Handles Slack Requests
   requestHandler(app, logger);
 
-// Add Slack Workflow Middleware
+  // Add Slack Workflow Middleware
   workflowHandler(app, logger);
 
-/**
- * App Entry Point
- */
-(async () => {
-  console.log("ENTRY === here -- ")
-  let http = require('http');
-  let server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello, World!\n');
-  });
-  server.listen(process.env.PORT || 7172);
-  console.log(`Server running on port ${process.env.PORT || 7172}`);
-  await app.start();
+  /**
+   * App Entry Point
+   */
+  (async () => {
+    console.log("ENTRY === here -- ")
+    let http = require('http');
+    let server = http.createServer(function (req, res) {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Hello, World!\n');
+    });
+    server.listen(process.env.PORT || 7172);
+    console.log(`Server running on port ${process.env.PORT || 7172}`);
+    await app.start();
 
-    await app.client.chat.postMessage({
-      channel: SLACK_CHANNEL,
-      text: `Platform Support Slack-bot server is running ${new Date()}`,
-      parse: 'full',
-      blocks: [{
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: `Platform Support Slack-bot server is running ${new Date()}`
-        }
-      }] 
-    })
+    let postStartup = async () => {
+      let authorized = {};
+      try {
+        authorized = await app.client.auth.test({token: SLACK_BOT_TOKEN});
+      } catch (error) {
+        authorized.ok = false;
+      }
+      
+      console.log('App authorization status', authorized);
+      if (authorized.ok) {
+        await app.client.chat.postMessage({
+          channel: SLACK_CHANNEL,
+          token: SLACK_BOT_TOKEN,
+          text: `Platform Support Slack-bot server is running ${new Date()}`,
+          parse: 'full',
+          blocks: [{
+            type: 'section',
+            text: {
+              type: 'mrkdwn',
+              text: `Platform Support Slack-bot server is running ${new Date()}`
+            }
+          }]
+        })
+      } else {
+        setTimeout(postStartup, 5000);
+      }
+    }
+    await postStartup();
 
     let warnProcessStop = async () => {
       await app.client.chat.postMessage({
         channel: SLACK_CHANNEL,
+        token: SLACK_BOT_TOKEN,
         text: `${SLACK_SUPPORT_TEAM_GROUP} Platform Support Slack-bot server is offline ${new Date()}`,
         parse: 'full',
         blocks: [{
@@ -93,7 +110,7 @@ if (SLACK_CHANNEL === undefined || SLACK_WEB_SOCKET_APP_TOKEN === undefined || S
             type: 'mrkdwn',
             text: `${SLACK_SUPPORT_TEAM_GROUP} Platform Support Slack-bot server is offline ${new Date()}`
           }
-        }] 
+        }]
       })
       return;
     }
